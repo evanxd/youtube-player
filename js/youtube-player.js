046963bdc81d852726821d27ca0597f6072a964c
Evan Xd <itoyxd@gmail.com>
2014-08-20 09:23:31 +0800

    Add videoid attribute

--- js/youtube-player.js
+++ js/youtube-player.js
@@ -1,12 +1,35 @@
-var template = document._currentScript.ownerDocument
-  .querySelector('template').content;
-var YoutubePlayer = Object.create(HTMLElement.prototype);
+var youtubePlayerDocument = document._currentScript.ownerDocument;
 
-YoutubePlayer.createdCallback = function() {
-  var shadowRoot = this.createShadowRoot();
-  shadowRoot.appendChild(document.importNode(template, true));
+function YoutubePlayer() {}
+
+YoutubePlayer.prototype = {
+  __proto__: HTMLElement.prototype,
+
+  _shadowRoot: null,
+
+  _videoId: null,
+
+  set videoId(videoid) {
+    this._videoId = videoid;
+    var player = this._shadowRoot.querySelector('#player');
+    player.src = player.src.replace('%videoid%', videoid);
+  },
+
+  get videoId() {
+    return this._videoId;
+  },
+
+  createdCallback: function() {
+    var template = youtubePlayerDocument.querySelector('#youtube-player').content;
+    this._shadowRoot = this.createShadowRoot();
+    this._shadowRoot.appendChild(document.importNode(template, true));
+
+    this.videoId = this.hasAttribute('videoid') ?
+      this.getAttribute('videoid') :
+      'VoLUvE-ny1k';
+  }
 };
 
-document.register("youtube-player", {
-  prototype: YoutubePlayer
+document.register('youtube-player', {
+  prototype: YoutubePlayer.prototype
 });
