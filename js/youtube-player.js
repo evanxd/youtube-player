cf55547664068eac7522427e5b537cfef6a6dedf
Evan Xd <itoyxd@gmail.com>
2014-08-20 23:29:22 +0800

    Add ready and statechange events

--- js/youtube-player.js
+++ js/youtube-player.js
@@ -5,18 +5,22 @@ function YoutubePlayer() {}
 YoutubePlayer.prototype = {
   __proto__: HTMLElement.prototype,
 
+  _player: null,
+
   _shadowRoot: null,
 
   _videoId: null,
 
   set videoId(videoid) {
     this._videoId = videoid;
-    var player = this._shadowRoot.querySelector('#player');
-    player.src = player.src.replace('%videoid%', videoid);
   },
 
   get videoId() {
     return this._videoId;
+  },
+
+  get currentTime() {
+    return this._player.getCurrentTime();
   },
 
   createdCallback: function() {
@@ -27,8 +31,44 @@ YoutubePlayer.prototype = {
     this.videoId = this.hasAttribute('videoid') ?
       this.getAttribute('videoid') :
       'VoLUvE-ny1k';
+
+    this._loadYoutubeApi();
+    youtubePlayerDocument.addEventListener('youtubeiframeapiready',
+      function() {
+        this._createPlayer();
+      }.bind(this));
+  },
+
+  _loadYoutubeApi: function() {
+    var script = document.createElement('script');
+    script.src = "https://www.youtube.com/iframe_api";
+    this._shadowRoot.appendChild(script);
+  },
+
+  _createPlayer: function() {
+    this._player = new YT.Player('player', {
+      videoId: this.videoId,
+      events: {
+        onReady: function(event) {
+          this.dispatchEvent(new CustomEvent('ready', {
+            detail: event
+          }));
+        }.bind(this),
+        onStateChange: function(event) {
+          this.dispatchEvent(new CustomEvent('statechange', {
+            detail: event
+          }));
+        }.bind(this)
+      }
+    });
   }
 };
+
+function onYouTubeIframeAPIReady() {
+  youtubePlayerDocument.dispatchEvent(
+    new CustomEvent('youtubeiframeapiready')
+  );
+}
 
 document.register('youtube-player', {
   prototype: YoutubePlayer.prototype
